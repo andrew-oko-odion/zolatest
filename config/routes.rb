06c2f22f4424014443aff9f46a977e3d41dea9fe
Andrew <andrew-oko-odion@gmail.com>
2018-09-30 21:08:51 +0100

    Update Rake Tasks and Add Routes For Swagger

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'countries#index'
+  mount Swaggard::Engine, at: '/api_docs/swagger/'
   namespace :api do
     namespace :v1 do
       resources :countries, only: [:index, :show]
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,6 +1,7 @@
 
-set :output, "/tmp/cron_log.log"
+set :output, {:error => "log/cron_error_log.log", :standard => "log/cron_log.log"}
 
- every 1.hour do
+ every 60.minutes do
    rake "pull_api_service_data:pull_data"
+   command "echo 'Local DB updated'"
  end
